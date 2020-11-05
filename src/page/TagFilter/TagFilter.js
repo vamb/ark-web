25ad32b3d45a5c6a5c09e844815d9bdff8aed87c
suyang <suyang.zhang@bwoil.com>
2020-11-05 19:19:28 +0800

    code update

--- src/page/TagFilter/TagFilter.js
+++ src/page/TagFilter/TagFilter.js
@@ -1,13 +1,9 @@
 import React from 'react'
 import SearchFilter from "./component/SearchFilter";
-import { Row, Col } from 'antd'
 
 const TagFilter = ()=>{
   return(
     <div>
-      <Row>
-        <Col span={24}>TagFilter</Col>
-      </Row>
       <SearchFilter />
     </div>
   )
